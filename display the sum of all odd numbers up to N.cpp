c9f1ffc812f09c516559b1e4bdeb864b8c8b83ab
MALESELALETSOALO <emjaymalesela@gmail.com>
2022-07-20 14:52:12 +0200

    Add project files.

---
+++ display the sum of all odd numbers up to N.cpp
@@ -0,0 +1,33 @@
+// display the sum of all odd numbers up to N.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    int i, num, odd_sum = 0, even_sum = 0;
+
+    cout << "Enter the value of num " << endl;
+    cin >> num;
+    for (i = 1; i <= num; i++)
+    {
+        if (i % 2 == 0)
+            even_sum = even_sum + i;
+        else
+            odd_sum = odd_sum + i;
+    }
+    cout << "Sum of all odd numbers  = "<<odd_sum << endl;
+    cout<< "Sum of all even numbers = " << even_sum << endl;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
